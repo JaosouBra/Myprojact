ab0138479642479798e0f3fd37fff43d69369ee6
JaosouBra <aticha0991862895@gmail.com>
2021-09-12 15:11:14 +0700

    pls help me

--- faceproject/lib/main.dart
+++ faceproject/lib/main.dart
@@ -17,3 +17,19 @@ void main() {
    );
    runApp(XDApp());
  }
+
+ class name extends StatefulWidget {
+   name({Key? key}) : super(key: key);
+ 
+   @override
+   _nameState createState() => _nameState();
+ }
+ 
+ class _nameState extends State<name> {
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+        child: null,
+     );
+   }
+ }
